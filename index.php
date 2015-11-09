7722f77fe5dbc0972b3b6b00edb7a99e97c2076b
Michiel Keijts <mkeijts@hotmail.com>
2015-11-09 14:42:21 +0100

    ryhdfghdfh

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ include ("functions.php");
 =======
 // HIER GAAT IETS GOED FOUT!
 >>>>>>> master
-if (function_exists("my_func")) {
+if (function_exists("my_funccgs ")) {
 	echo "My Function Exists";
 } else {
 	echo "My Function Does not Exists?";
