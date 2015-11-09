54dae66c5509f4196dfc8d24d62a5427827bb305
Michiel Keijts <mkeijts@hotmail.com>
2015-11-09 13:52:41 +0100

    Updated index language

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php 
-
+// HIER GAAT IETS GOED FOUT!
 if (function_exists("my_predef_function")) {
 	echo "My Function Exists";
 } else {
-	echo "My Function Does not Exists";
+	echo "My Function Does not Exists, Really?";
 }
 ?>
